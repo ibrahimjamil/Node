91522a3ac71cc551c6c31e84a25fdb831f8e29ca
ibrahimjamil <ibrahimjamil147@ucp.edu.pk>
2022-05-02 13:49:20 +0500

    added redis cache

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '795428594411-pvlug9t47fh4eapv0o08eb1lbgm7g571.apps.googleusercontent.com',
+  googleClientSecret: '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',
+  mongoURI: 'mongodb://ibrahimjamil:Ibrahimjamil123@cluster0-shard-00-00.pkflc.mongodb.net:27017,cluster0-shard-00-01.pkflc.mongodb.net:27017,cluster0-shard-00-02.pkflc.mongodb.net:27017/blog_everyone?ssl=true&replicaSet=atlas-gw3ojk-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
---
+++ services/cache.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const redis = require('redis');
+const utils = require('util')
+
+const redisUrl = 'redis://127.0.0.1:6379'
+const client = redis.createClient(redisUrl);
+client.hget = utils.promisify(client.hget);
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.cache = function (options = {}) {
+    this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
+    return this;
+}
+
+mongoose.Query.prototype.exec = async function () {
+    if (!this.useCache) {
+        return await exec.apply(this, arguments);
+    }
+
+    const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    }))
+
+    const cachedValue = await client.hget(this.hashKey, key)
+    if (cachedValue) {
+        // when we add new data we must flush previous cache by doing client.flushall();
+        console.log("from cache")
+        const doc = JSON.parse(cachedValue);
+        return Array.isArray(doc)
+         ? doc.map((d) => new this.model(d))
+         : new this.model(doc);
+    }
+
+    const result = await exec.apply(this, arguments);
+    client.hset(this.hashKey, key,JSON.stringify(result));
+    return result;
+}
+
+const clearHash = (hashKey) => {
+    client.del(JSON.stringify(hashKey));
+}
+
+module.exports = {
+    clearHash
+};
